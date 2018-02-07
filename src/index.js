14ceca820fe2a508c2172dc77d9c1795a303705a
elzup <guild0105@gmail.com>
2018-02-07 15:36:42 +0900

    Remove material-ui old beta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import App from './containers/App'
 import registerServiceWorker from './config/registerServiceWorker'
@@ -21,9 +20,7 @@ const rootEl = document.getElementById('root')
 if (rootEl !== null) {
 	ReactDOM.render(
 		<Provider store={store}>
-			<MuiThemeProvider>
-				<App />
-			</MuiThemeProvider>
+			<App />
 		</Provider>,
 		rootEl,
 	)
