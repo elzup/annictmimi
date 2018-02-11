2379bcde42756fa06202a1940cbe363a76be3ab5
elzup <guild0105@gmail.com>
2018-02-11 15:08:09 +0900

    Fix My episode Timestamp

--- src/containers/MainEpisodeContainer/reducer.js
+++ src/containers/MainEpisodeContainer/reducer.js
@@ -10,7 +10,7 @@ export const initialState: State = []
 export default function(state: State = initialState, action: Action): State {
 	switch (action.type) {
 		case Actions.RECEIVE_MAIN_EPISODE:
-			return _.uniq([...state, ...action.episodes.map(r => r.id)])
+			return _.uniq([...action.episodes.map(r => r.id)])
 
 		default:
 			return state
